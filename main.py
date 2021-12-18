7587eaaadb276fc18f7705df42f973246176aa82
Neil Muglurmath <neil.muglurmath@gmail.com>
2021-12-18 13:59:21 -0500

    download album img

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 from secrets import *
+import requests
+import shutil
 
 # get username from terminal
 scope = 'user-read-private user-read-playback-state user-modify-playback-state'
@@ -25,6 +27,8 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 currPlaylists = sp.current_user_playlists()
 saucePlaylist = currPlaylists['items'][0]['id']
 saucePlaylist = sp.playlist(saucePlaylist)['tracks']['items']
+# saucePlaylist = sorted(
+# saucePlaylist, key=lambda k: k['added_at'], reverse=True)
 
 albumArt = []
 albumIDOccur = {}
@@ -39,4 +43,14 @@ for song in saucePlaylist:
         albumIDOccur[albumID] = 1
         albumIDsToArtURL[albumID] = song['track']['album']['images'][0]['url']
 
-# print(json.dumps(VARIABLE, sort_keys=True,indent=4))
+albumIDOccur = {k: v for k, v in sorted(
+    albumIDOccur.items(), key=lambda item: item[1], reverse=True)}
+albumIDOccur = list(albumIDOccur.items())
+
+for i in albumIDOccur:
+    filename = albumIDsToArtURL[i[0]].split("/")[-1]
+    r = requests.get(albumIDsToArtURL[i[0]], stream=True)
+    r.raw.decode_content = True
+    with open(filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+    # print(json.dumps(VARIABLE, sort_keys=True,indent=4))
